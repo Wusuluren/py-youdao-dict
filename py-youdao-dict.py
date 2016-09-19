910ff29261a7a2798e064b2169ff7867f2630234
post <weixiaoyiri@gmail.com>
2016-09-19 22:17:57 +0800

    初步实现GUI下的查询单词功能

--- py-youdao-dict.py
+++ py-youdao-dict.py
@@ -11,28 +11,200 @@ import time
 import collections
 from tkinter import *
 
+#GUI程序
 class Application(object):
-    def __init__(self):
-        self.top = Tk()
-        self.top.title('Hello')
-        self.CreateWidgets()
-        self.top.mainloop()
+	def __init__(self):
+		self.top = Tk()
+		self.top.wm_attributes('-topmost', 1)
+		self.top.title('py-youdao-dict')
+		self.CreateWidgets()
+		self.InitApp()
+		self.top.mainloop()
 
-    def CreateWidgets(self):
-        self.userText = Entry(self.top)
-        self.userText.grid(row=0, column=0)
+	def InitApp(self):
+		#有道词典的OPEN API
+		self.API_KEY = '1437381740'
+		self.KEYFROM = 'py-youdao-dict'
 
-        self.searchButton = Button(self.top, text=u'查询', command=self.Search)
-        self.searchButton.grid(row=0, column=1)
+		self.spellCorrector = SpellCorrector()
+		self.predictor = Predictor()
+		self.fileSets = [[] for i in range(26)]
 
-        self.translateText = Text(self.top)
-        self.translateText.grid(row=1, column=0, columnspan=2)
+		self.LoadSavedFile()
 
-    def Search(self):
-        self.translateText.delete(0.0, END)
-        self.translateText.insert(0.0, 'Hello')
+	def CreateWidgets(self):
+		self.userText = Entry(self.top)
+		self.userText.grid(row=0, column=0)
 
- class SpellCorrector(object):
+		self.searchButton = Button(self.top, text=u'查询', command=self.Search)
+		self.searchButton.grid(row=0, column=1)
+
+		self.translateText = Text(self.top)
+		self.translateText.grid(row=1, column=0, columnspan=2)
+
+	def Search(self):
+		txt = self.userText.get().lower()
+		translate = self.Sjson(self.GetTranslate(txt))
+		self.translateText.delete(0.0, END)
+		self.translateText.insert(0.0, translate)
+
+	def LoadSavedFile(self):
+		try:
+			filepath = r'py-youdao-dict.json'
+			fp = open(filepath, 'r+')
+		except FileNotFoundError:
+			print(u'%s文件不存在' %filepath)	
+			return
+		fp.seek(os.SEEK_SET)
+		file = fp.readlines()
+		file.sort(key=lambda x:json.loads(x).get('query', ''))
+		#for line in file:
+		#	print(line)
+		fp.close()
+		
+		for line in file:
+			currentQuery = json.loads(line).get('query', '')
+			setIdx = ord(currentQuery.lower()[0]) - ord('a')
+			self.fileSets[setIdx].append(json.loads(line))
+
+	'''
+		for i in self.fileSets:
+			if i != []:
+				print('*' * 40)
+				for j in i:
+					print(j)
+	'''		
+
+	def GetTranslateFromFile(self, query):
+		query = query.lower()
+		findWord = False
+		setIdx = ord(query[0]) - ord('a')
+		if [] != self.fileSets[setIdx]:
+			for wordSaved in self.fileSets[setIdx]:
+				if query == wordSaved.get('query', ''):
+					findWord = True
+					break
+				
+		if findWord:	
+			return wordSaved
+		else:
+			return None	
+
+	def SaveWordToFile(self, query, jsonData):
+		query = query.lower()
+		choices = 'y'
+		if choices in ['y', 'Y']:
+			alreadySaved = False
+			setIdx = ord(query[0]) - ord('a')
+			#print(self.fileSets[setIdx])
+			if [] == self.fileSets[setIdx]:
+				self.fileSets[setIdx].append(jsonData)
+			else:	
+				for wordSaved in self.fileSets[setIdx]:
+					if query == wordSaved.get('query', ''):
+						alreadySaved = True
+						break
+
+			if alreadySaved:
+				print(u'单词已经在单词本\r\n')
+			else:
+				self.fileSets[setIdx].append(jsonData)
+				filepath = r'py-youdao-dict.json'
+				fp = open(filepath, 'a+')
+				fp.write(json.dumps(jsonData))
+				fp.write('\r\n')
+				fp.close()
+				print(u'写入单词本成功\r\n')
+
+	def GetTranslate(self, query):
+		query = query.lower()
+		url = 'http://fanyi.youdao.com/openapi.do'
+		data = {
+			'keyfrom': self.KEYFROM,
+			'key': self.API_KEY,
+			'type': 'data',
+			'doctype': 'json',
+			'version': 1.1,
+			'q': query
+		}
+		data = parse.urlencode(data)
+		url = url+'?'+data
+		req = request.Request(url)
+		offline = False
+		try:
+			response = request.urlopen(req)
+		except error.URLError:
+			offline = True
+			result = GetTranslateFromFile(query)
+		if not offline:	
+			result = json.loads(response.read())
+		return result					
+
+	def Sjson(self, jsonData):
+		if None == jsonData:
+			#print(u'单词本中没有收录该单词，请联网查询！\r\n')
+			return u'单词本中没有收录该单词，请联网查询！\r\n'
+
+		query = jsonData.get('query', '')
+		translation = jsonData.get('translation', '')
+		basic = jsonData.get('basic', '')
+		if basic == '':
+			#print(u'查询单词出现错误')
+			return u'查询单词出现错误'
+
+		sequence = jsonData.get('web', [])	
+		phonetic, explains_txt, seq_txt, log_word_explains = \
+			'', '', '', ''
+
+		if basic:
+			phonetic = basic.get('phonetic', '')
+			explains = basic.get('explains', '')
+			for obj in explains:
+				explains_txt += obj+'\n'
+				log_word_explains += obj+','
+
+		if sequence:
+			for obj in sequence:
+				seq_txt += obj['key']+'\n'
+				values = ''
+				for i in obj['value']:
+					values += i+','
+				seq_txt += values+'\n'
+
+		result = ''
+		result += u'查询对象： %s [%s]\n' %(query, phonetic)
+		result += explains_txt
+		result += '-'*20+'\n'+seq_txt	
+
+		self.SaveWordToFile(query, jsonData)
+
+		return result	
+
+	def WordbookSample(self):
+		with open('wordbook-sample.txt', 'r') as fp:
+			print(u'单词本开始收录')	
+			file = fp.readlines()
+			txtCounter = 0
+			for line in file:
+				wordbookSample = list(filter(lambda x:x!= '', re.split(r'[^a-zA-Z]+', line)))
+				for txt in wordbookSample:
+					txtCounter += 1
+					if 0 == (txtCounter & 15):
+						time.sleep(1)
+					#使用API查询，请求频率限制为每小时1000次，超过限制会被封禁	
+					if txtCounter >= 900:
+						return	
+					Sjson(GetTranslate(txt.lower()))
+			print(u'单词本收录完毕')	
+
+
+
+
+
+
+
+#拼写检查
+class SpellCorrector(object):
 	def __init__(self):
 		self.NWORDS = self.train(self.words(open('big.txt', 'r').read()))
 		self.alphabet = 'abcdefghijklmnopqrstuvwxyz'
@@ -64,6 +236,7 @@ class Application(object):
 		candidates = self.known([word]) or self.known(self.edits1(word)) or self.known_edits2(word) or [word]
 		return max(candidates, key=self.NWORDS.get)
 
+#用户输入预测
 class Predictor(object):
 	def __init__(self):
 		with open('big.txt', 'r') as f:
@@ -74,160 +247,15 @@ class Predictor(object):
 
 	def SetModel(self, text):
 		return text
-
-API_KEY = '1437381740'
-KEYFROM = 'py-youdao-dict'
-
-global g_fileSets
-
-g_fileSets = [[] for i in range(26)]
-
-def LoadSavedFile():
-	global g_fileSets
-
-	try:
-		filepath = r'py-youdao-dict.json'
-		fp = open(filepath, 'r+')
-	except FileNotFoundError:
-		print(u'%s文件不存在' %filepath)	
-		return
-	fp.seek(os.SEEK_SET)
-	file = fp.readlines()
-	file.sort(key=lambda x:json.loads(x).get('query', ''))
-	#for line in file:
-	#	print(line)
-	fp.close()
-	
-	for line in file:
-		currentQuery = json.loads(line).get('query', '')
-		setIdx = ord(currentQuery.lower()[0]) - ord('a')
-		g_fileSets[setIdx].append(json.loads(line))
-
-	'''
-	for i in g_fileSets:
-		if i != []:
-			print('*' * 40)
-			for j in i:
-				print(j)
-	'''
-
-def GetTranslateFromFile(query):
-	global g_fileSets
-
-	query = query.lower()
-	findWord = False
-	setIdx = ord(query[0]) - ord('a')
-	if [] != g_fileSets[setIdx]:
-		for wordSaved in g_fileSets[setIdx]:
-			if query == wordSaved.get('query', ''):
-				findWord = True
-				break
-			
-	if findWord:	
-		return wordSaved
-	else:
-		return None	
-
-def SaveWordToFile(query, jsonData, cmdDict):
-	global g_fileSets
-
-	query = query.lower()
-	if not cmdDict['noSave']:
-		choices = 'y'
-		if not cmdDict['autoSave']:
-			choices = input(u'是否写入单词本，回复(y/n):')
-		if choices in ['y', 'Y']:
-			alreadySaved = False
-			setIdx = ord(query[0]) - ord('a')
-			#print(g_fileSets[setIdx])
-			if [] == g_fileSets[setIdx]:
-				g_fileSets[setIdx].append(jsonData)
-			else:	
-				for wordSaved in g_fileSets[setIdx]:
-					if query == wordSaved.get('query', ''):
-						alreadySaved = True
-						break
-
-			if alreadySaved:
-				print(u'单词已经在单词本\r\n')
-			else:
-				g_fileSets[setIdx].append(jsonData)
-				filepath = r'py-youdao-dict.json'
-				fp = open(filepath, 'a+')
-				fp.write(json.dumps(jsonData))
-				fp.write('\r\n')
-				fp.close()
-				print(u'写入单词本成功\r\n')
-
-
-def GetTranslate(query):
-	query = query.lower()
-	url = 'http://fanyi.youdao.com/openapi.do'
-	data = {
-		'keyfrom': KEYFROM,
-		'key': API_KEY,
-		'type': 'data',
-		'doctype': 'json',
-		'version': 1.1,
-		'q': query
-	}
-	data = parse.urlencode(data)
-	url = url+'?'+data
-	req = request.Request(url)
-	offline = False
-	try:
-		response = request.urlopen(req)
-	except error.URLError:
-		offline = True
-		result = GetTranslateFromFile(query)
-	if not offline:	
-		result = json.loads(response.read())
-	return result
-
-def Sjson(jsonData, cmdDict):
-	if None == jsonData:
-		print(u'单词本中没有收录该单词，请联网查询！\r\n')
-		return
-
-	query = jsonData.get('query', '')
-	translation = jsonData.get('translation', '')
-	basic = jsonData.get('basic', '')
-	if basic == '':
-		print(u'查询单词出现错误')
-		return
-
-	sequence = ''
-	if not cmdDict['simpleMode']:	
-		sequence = jsonData.get('web', [])	
-	phonetic, explains_txt, seq_txt, log_word_explains = \
-		'', '', '', ''
-
-	if basic:
-		phonetic = basic.get('phonetic', '')
-		explains = basic.get('explains', '')
-		for obj in explains:
-			explains_txt += obj+'\n'
-			log_word_explains += obj+','
-
-	if sequence:
-		for obj in sequence:
-			seq_txt += obj['key']+'\n'
-			values = ''
-			for i in obj['value']:
-				values += i+','
-			seq_txt += values+'\n'
-
-	print_format = ''
-	print_format += '*'*40+'\n'
-	print_format += u'查询对象： %s [%s]\n' %(query, phonetic)
-	print_format += explains_txt
-	print_format += '-'*20+'\n'+seq_txt
-	print_format += '*'*40+'\n'
-	print(print_format, end='')
-
-	SaveWordToFile(query, jsonData, cmdDict)
 		
 
+
+
+
+
+
+
+'''
 def Usage(cmdDict):
 	print('*' * 40)
 	print(u'0:显示帮助')
@@ -238,7 +266,9 @@ def Usage(cmdDict):
 	print(u'4:打开/关闭简洁模式 - 当前%s模式' \
 		%(u'打开' if cmdDict['simpleMode'] else u'关闭') )
 	print('*' * 40 + '\r\n')
+'''
 
+'''
 def DecodeCommand(cmd, usageDict, cmdDict):
 	if 'showUsage' == usageDict[cmd]:
 		Usage(cmdDict)
@@ -253,28 +283,11 @@ def DecodeCommand(cmd, usageDict, cmdDict):
 		if 'noSave' == usageDict[cmd]:
 			cmdDict['autoSave'] = False
 			cmdDict['noSave'] = True	
-
-def WordbookSample(cmdDict):
-	with open('wordbook-sample.txt', 'r') as fp:
-		print(u'单词本开始收录')	
-		file = fp.readlines()
-		txtCounter = 0
-		for line in file:
-			wordbookSample = list(filter(lambda x:x!= '', re.split(r'[^a-zA-Z]+', line)))
-			for txt in wordbookSample:
-				txtCounter += 1
-				if 0 == (txtCounter & 15):
-					time.sleep(1)
-				#使用API查询，请求频率限制为每小时1000次，超过限制会被封禁	
-				if txtCounter >= 900:
-					return	
-				Sjson(GetTranslate(txt.lower()), cmdDict)
-		print(u'单词本收录完毕')	
+'''
 
 
-def main():
-	app = Application()
-	
+'''
+def console_main():
 	usageDict = {
 		u'0': 'showUsage', 
 		u'1': 'quitProgram',
@@ -293,7 +306,7 @@ def main():
 	spellCorrector = SpellCorrector()
 
 	predictor = Predictor()
-	print(predictor.Predict('hel'))
+	#print(predictor.Predict('hel'))
 
 	LoadSavedFile()	
 	#WordbookSample(cmdDict)
@@ -316,6 +329,10 @@ def main():
 					elif correctChoice in ['r', 'R']:
 						continue
 				Sjson(GetTranslate(txt.lower()), cmdDict)
+'''
+
+def gui_main():
+	app = Application()
 
 if __name__ == '__main__':
-	main()
+	gui_main()
